cdfe775dc7374247c150ab77887be168a6342be2
MohamedShamry <miswer.2019812@iit.ac.lk>
2022-07-02 20:19:52 +0530

    new changes

--- frontendApp/airbnb-frontend/pages/index.js
+++ frontendApp/airbnb-frontend/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div >
-        <h1>this is the air bnb front end updated </h1>
+        <h1>this is the air bnb front end updatfffed </h1>
     </div>
   )
 }
